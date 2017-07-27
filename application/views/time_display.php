7909d29cc401746d8fda74be4ac9dc0c153c69df
jesus <chuyelregio5@yahoo.com>
2017-07-26 20:36:29 -0700

    first commit

---
+++ application/views/time_display.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Time and Date</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<style type="text/css">
+		html {
+		  text-align: center;
+		}
+		h1 {
+			font-size: 34px;
+			font-family: 'Open Sans', sans-serif;
+		}
+		h2 {
+			font-family: 'Open Sans', sans-serif;
+		}
+	</style>
+</head>
+<body>
+	<div id="container">
+		<h1>The current Time and Date: </h1>
+		<h2>The Time: <?php echo $time ?></h2>
+		<h2>The Date: <?php echo $date ?></h2>
+	</div>
+</body>
+</html>
